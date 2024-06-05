89d39770d93ac5a3309c74c1f291af4ea2515d62
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-05 15:12:48 +0630

    temporary changes

--- tutorial/Program.cs
+++ tutorial/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int diamondLenght = 11;
+            int i, y;
+             for( i = 0; i <= diamondLenght; i++)
+             {
+                if (i <= (diamondLenght / 2))
+                {
+                    for ( y = 0; y <= diamondLenght; y++)
+                    { 
+                        
+                        Console.Write("*");
+                    }    
+                    Console.WriteLine();
+                }
+                else
+                {
+                   
+                }  
+
+             } 
+        }
+    }
+}
