97ca903f15b496ec27779ba442be8afdccd9aa7c
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-05 16:23:09 +0630

    leap year commit

--- tutorial/Program.cs
+++ tutorial/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public static void getCenturyAndLeapYear(int year)
+        {
+            int leap, century;
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            {
+                leap = 1;
+            }
+            else
+            {
+                leap = -1;
+            }
+            century = (year / 100) + 1;
+            Console.WriteLine("Output : "+ century +", "+ leap );
+        }
+        static void Main(string[] args)
+        {
+            int year;
+            Console.Write("Input Year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            getCenturyAndLeapYear(year);
+        }
+    }
+}
