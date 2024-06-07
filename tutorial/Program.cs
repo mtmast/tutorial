2d42a2865d3a911e15ad28c767c1bb7fa4024068
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-07 10:45:09 +0630

    review fix 01

--- tutorial/Program.cs
+++ tutorial/Program.cs
@@ -6,7 +6,7 @@
         {
             int leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ? 1 : -1;
             int century = (year >= 1000 ) ?  (year / 100) + 1 : -1;
-            Console.WriteLine("Output : "+ century +", "+ leap );
+            Console.WriteLine($"Output : {century}, {leap} ");
         }
         static void Main(string[] args)
         {
