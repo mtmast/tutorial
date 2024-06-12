175bdf6ede83cbc8cf04f2907dbd7b069e1f87aa
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-12 16:56:45 +0630

    Commit Something Not Special

--- ImportExport/Controllers/RentController.cs
+++ ImportExport/Controllers/RentController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ImportExport.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.IO;
 
@@ -66,13 +70,86 @@ namespace ImportExport.Controllers
                         Debug.Write("Your File" + FilePath);
                         if (System.IO.File.Exists(FilePath))
                         {
-                            ModelState.AddModelError("", "Your File is Already Exist");
+                            TempData["MessageType"] = "fail";
+                            TempData["Message"] = "Your File is Already Exist";
                             return View("Index"); // Return to the view with the error message
                         }
-                        using (var stream = System.IO.File.Create(FilePath))
+                        using (var streams = System.IO.File.Create(FilePath))
                         {
-                            await SingleFile.CopyToAsync(stream);
+                            await SingleFile.CopyToAsync(streams);
                         }
+
+                        // import to database 
+                        var stream = SingleFile.OpenReadStream();
+                        var customers = new List<TblCustomer>();
+                        var renters = new List<TblRent>();
+                        var movies = new List<TblMovie>();
+                        int TemporaryId;
+                        try
+                        {
+                            using (var package = new ExcelPackage(stream))
+                            {
+                                var worksheet = package.Workbook.Worksheets.First();//package.Workbook.Worksheets[0];
+                                var rowCount = worksheet.Dimension.Rows;
+
+                                for (var row = 2; row <= rowCount; row++)
+                                {
+                                    TemporaryId = Convert.ToInt32(DateTime.Now.Ticks % 100000000) ;
+                                    try
+                                    {
+
+                                        string movieTitle = worksheet?.Cells[row, 3].Value?.ToString();
+                                        customers.Add(
+                                           new TblCustomer()
+                                           {
+                                               CusId = TemporaryId,
+                                               FullName = worksheet.Cells[row, 1].Value?.ToString(),
+                                               Salutation = worksheet.Cells[row, 4].Value?.ToString(),
+                                               Address = worksheet.Cells[row, 2].Value?.ToString(),
+                                               CreatedAt = DateTime.Now
+                                           }
+                                       );
+                                        
+                                        using (var context = new DBContext())
+                                        {
+                                          
+                                            int movieId = await context.TblMovies
+                                                            .Where(c => c.Title == movieTitle)
+                                                            .Select(d => d.MvId) 
+                                                            .FirstOrDefaultAsync();
+                                            Debug.Write("TBLTBLTBLMOVIE-------=============" + movieId);
+
+                                            if (movieId > 0)
+                                            {
+                                                renters.Add(
+                                                    new TblRent()
+                                                    {
+                                                        RentId = TemporaryId,
+                                                        CusId = TemporaryId,
+                                                        MvId = movieId, 
+                                                        RentAt = DateTime.Now
+                                                    }
+                                                );
+                                                context.Add(renters);
+                                                context.Add(customers);
+                                                await context.SaveChangesAsync();
+                                            }
+
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Debug.Write("Errrrrrrrrrrrrrrrrrrrrrr From Inner Try" + ex);
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.Write("Errrrrrrrrrrrrrrrrrrrrrr" + e);
+                            return View("Index");
+                        }
+
                         return RedirectToAction(nameof(Index));
                     }
                 }
