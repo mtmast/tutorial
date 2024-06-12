41c95675007da70520060fa9f1e2c2a8f270fe06
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-12 10:48:13 +0630

    delete unnecessary file

--- AuthCookie/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AuthCookie.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
