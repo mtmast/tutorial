ffe4a015e47413aa44ce628aebccc4de7af8b58d
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-13 16:58:30 +0630

    frame setup

---
+++ ResetPwd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ResetPwd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
