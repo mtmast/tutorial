bed72ed0ea7d5a3ee454e3ec91825c3af89b61f3
Aung Si Thu (MTM) <mtmintern.aungsithu@gmail.com>
2024-06-13 16:11:22 +0630

    delete unnecessary file

--- ImportExport/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ImportExport.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
